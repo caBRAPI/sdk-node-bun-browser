9c974efcea6c0e25db8606a2586b0565f287b9af
sebastianjnuwu <sebastianjnuwu@gmail.com>
2026-05-25 11:36:45 -0300

    Update package version to 1.0.1 and enhance description to include Bun support; add comments for Node.js and Browser imports in client.js

--- example/src/client.js
+++ example/src/client.js
@@ -1,6 +1,9 @@
 import { caBRAPI } from "../../dist/index.js";
 
+// Node.js
 // import { caBRAPI } from '@cabrapi/sdk';
+
+// Browser e Bun
 // import { caBRAPI } from 'https://cdn.jsdelivr.net/npm/@cabrapi/sdk/dist/index.js';
 
 const client = new caBRAPI({
